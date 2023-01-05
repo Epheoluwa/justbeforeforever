ce6b9314c2233e1966279c734589024bdf4a699c
Solomon Sunmola <sunmolasolomon@gmail.com>
2023-01-05 23:53:57 +0100

    date update

--- src/App.vue
+++ src/App.vue
@@ -39,8 +39,8 @@
                             first half of 2023</p>
                         <a class="btn-solid-lg" href="https://forms.gle/czYbRBeC16jaEiZd9" target="_blank">Register
                             Now</a>
-                        <h4 class="mt-4">Start: January 29, 2023 </h4>
-                        <h4 class="mt-4">Fee- Affordable </h4>
+                        <h4 class="mt-4">Start: February 19th, 2023 </h4>
+                        <h4 class="mt-4">Fee- &#8358;25,000 </h4>
                     </div>
                 </div>
                 <!-- <div class="col-lg-6 col-xl-7">
